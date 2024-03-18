38c2f0c5ca535e5edec5a849edcdee666c27b81b
Muhammedfarhan707 <muhammedfarhan0285@gmail.com>
2024-03-18 12:41:24 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CounterMain from './components/Countermain'
+
+function App() {
+ 
+  return (
+    <>
+      <CounterMain/>
+    </>
+  )
+}
+
+export default App
---
+++ src/redux/slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const counterSlice=createSlice({
+    name:'Counter',
+    initialState:{
+        value:0
+    },
+    reducers:{
+        increase:(state)=>{
+            state.value+=1
+        },
+        decrease:(state)=>{
+            state.value-=1
+        },
+        reset:(state)=>{
+            state.value=0
+        }
+    }
+})
+
+export default counterSlice.reducer
+export const {increase,decrease,reset}=counterSlice.actions
